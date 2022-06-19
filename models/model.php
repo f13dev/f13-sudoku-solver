081fb2d2677e714a2dc304228eff43774e4da37f
James Valentine <jv@f13dev.com>
2022-06-19 12:58:49 +0100

    Refactor: Variable naming conventions

--- models/model.php
+++ models/model.php
@@ -2,19 +2,19 @@
 
 class Model
 {
-    private $x;  // x axis
-    private $y;  // y axis
-    private $c;  // cubes
-    private $p;  // puzzle
-    private $s;  // solved
+    private $columns;  // x axis
+    private $rows;  // y axis
+    private $squares;  // cubes
+    private $puzzle;  // puzzle
+    private $solved;  // solved
 
     public function __construct($sudoku)
     {
-        $this->p = $sudoku;
-        $this->s = $sudoku;
-        $this->_set_y();
-        $this->_set_x();
-        $this->_set_c();
+        $this->puzzle = $sudoku;
+        $this->solved = $sudoku;
+        $this->_set_rows();
+        $this->_set_columns();
+        $this->_set_squares();
     }
 
     /**
@@ -26,11 +26,11 @@ class Model
     public function validate_puzzle()
     {
         // Check that each number appears only once in rows
-        foreach ($this->y as $row) {
-            for ($n = 1; $n <= 9; $n++) {
+        foreach ($this->rows as $row) {
+            for ($number = 1; $number <= 9; $number++) {
                 $count = 0;
                 foreach ($row as $cell) {
-                    if ($cell == $n) {
+                    if ($cell == $number) {
                         $count++;
                         if ($count > 1) {
                             return false;
@@ -41,11 +41,11 @@ class Model
         }
 
         // Check that each number appears only once in cols
-        foreach ($this->x as $col) {
-            for ($n = 1; $n <= 9; $n++) {
+        foreach ($this->columns as $column) {
+            for ($number = 1; $number <= 9; $number++) {
                 $count = 0;
-                foreach ($col as $cell) {
-                    if ($cell == $n) {
+                foreach ($column as $cell) {
+                    if ($cell == $number) {
                         $count++;
                         if ($count > 1) {
                             return false;
@@ -56,11 +56,11 @@ class Model
         }
 
         // Check that each number appears only once in cubes
-        foreach ($this->c as $square) {
-            for ($n = 1; $n <= 9; $n++) {
+        foreach ($this->squares as $square) {
+            for ($number = 1; $number <= 9; $number++) {
                 $count = 0;
                 foreach ($square as $cell) {
-                    if ($cell == $n) {
+                    if ($cell == $number) {
                         $count++;
                         if ($count > 1) {
                             return false;
@@ -83,34 +83,34 @@ class Model
      *     Int x column coordinate
      *     Int y row coordinate
      */
-    public function _get_yx_from_c($cube, $cell)
+    public function _get_row_column_from_square_cell($square, $cell)
     {
-        switch ($cube) {
-            case 1: case 4: case 7: $xs = array(1,2,3); break;
-            case 2: case 5: case 8: $xs = array(4,5,6); break;
-            case 3: case 6: case 9: $xs = array(7,8,9); break;
+        switch ($square) {
+            case 1: case 4: case 7: $square_columns = array(1,2,3); break;
+            case 2: case 5: case 8: $square_columns = array(4,5,6); break;
+            case 3: case 6: case 9: $square_columns = array(7,8,9); break;
         }
-        switch ($cube) {
-            case 1: case 2: case 3: $yx = array(1,2,3); break;
-            case 4: case 5: case 6: $ys = array(4,5,6); break;
-            case 7: case 8: case 9: $ys = array(7,8,9); break;
-        }
-
-        switch ($cell) {
-            case 1: case 2: case 3: $x = $xs[0]; break;
-            case 4: case 5: case 6: $x = $xs[1]; break;
-            case 7: case 8: case 9: $x = $xs[2]; break;
+        switch ($square) {
+            case 1: case 2: case 3: $square_rows = array(1,2,3); break;
+            case 4: case 5: case 6: $square_rows = array(4,5,6); break;
+            case 7: case 8: case 9: $square_rows = array(7,8,9); break;
         }
 
         switch ($cell) {
-            case 1: case 4: case 7: $y = $ys[0]; break;
-            case 2: case 5: case 8: $y = $ys[1]; break;
-            case 3: case 6: case 8: $y = $ys[2]; break;
+            case 1: case 2: case 3: $column = $square_columns[0]; break;
+            case 4: case 5: case 6: $column = $square_columns[1]; break;
+            case 7: case 8: case 9: $column = $square_columns[2]; break;
+        }
+
+        switch ($cell) {
+            case 1: case 4: case 7: $row = $square_rows[0]; break;
+            case 2: case 5: case 8: $row = $square_rows[1]; break;
+            case 3: case 6: case 8: $row = $square_rows[2]; break;
         }
 
         return array(
-            'x' => $x,
-            'y' => $y,
+            'column' => $column,
+            'row' => $row,
         );
     }
 
@@ -131,14 +131,14 @@ class Model
      *         Int y Cell y position
      *         Int x Cell x position
      */
-    public function _get_c_id($y, $x)
+    public function _get_square_from_row_column($row, $column)
     {
-        switch ($y) {
+        switch ($row) {
             case 1: case 2: case 3: $cy = 1; $cys = array(1, 2, 3); break;
             case 4: case 5: case 6: $cy = 2; $cys = array(4, 5, 6); break;
             case 7: case 8: case 9: $cy = 3; $cys = array(7, 8, 9); break;
         }
-        switch ($x) {
+        switch ($column) {
             case 1: case 2: case 3: $cx = 0; $cxs = array(1, 2, 3); break;
             case 4: case 5: case 6: $cx = 3; $cxs = array(4, 5, 6); break;
             case 7: case 8: case 9: $cx = 6; $cxs = array(7, 8, 9); break;
@@ -146,12 +146,12 @@ class Model
 
         $cube = $cy+$cx;
 
-        switch ($y) {
+        switch ($row) {
             case 1: case 4: case 7: $cy = 1; break;
             case 2: case 5: case 8: $cy = 2; break;
             case 3: case 6: case 9: $cy = 3; break;
         }
-        switch ($x) {
+        switch ($column) {
             case 1: case 4: case 7: $cx = 0; break;
             case 2: case 5: case 8: $cx = 3; break;
             case 3: case 6: case 9: $cx = 6; break;
@@ -185,8 +185,8 @@ class Model
      */
     public function is_solved()
     {
-        for ($y = 1; $y <= 9; $y++) {
-            if (array_sum($this->s[$y]) != 45) {
+        for ($row = 1; $row <= 9; $row++) {
+            if (array_sum($this->solved[$row]) != 45) {
                 return false;
             }
         }
@@ -196,12 +196,12 @@ class Model
     /**
      * Set the locally stored x axis array
      */
-    public function _set_x()
+    public function _set_columns()
     {
-        $this->x = array();
-        for ($x = 1; $x <= 9; $x++) {
-            for ($y = 1; $y <= 9; $y++) {
-                $this->x[$x][$y] = $this->p[$y][$x];
+        $this->columns = array();
+        for ($column = 1; $column <= 9; $column++) {
+            for ($row = 1; $row <= 9; $row++) {
+                $this->columns[$column][$row] = $this->puzzle[$row][$column];
             }
         }
     }
@@ -209,21 +209,21 @@ class Model
     /**
      * Set the locally stored y axis array
      */
-    public function _set_y()
+    public function _set_rows()
     {
-        $this->y = $this->p;
+        $this->rows = $this->puzzle;
     }
 
     /**
      * Set the locally stored square array
      */
-    public function _set_c()
+    public function _set_squares()
     {
-        $this->c = array();
-        for($y = 1; $y <= 9; $y++) {
-            for($x = 1; $x <= 9; $x++) {
-                $c = $this->_get_c_id($y, $x);
-                $this->c[$c['cube']][$c['cell']] = $this->p[$y][$x];
+        $this->squares = array();
+        for($row = 1; $row <= 9; $row++) {
+            for($column = 1; $column <= 9; $column++) {
+                $c = $this->_get_square_from_row_column($row, $column);
+                $this->squares[$c['cube']][$c['cell']] = $this->puzzle[$row][$column];
             }
         }
     }
@@ -239,19 +239,19 @@ class Model
     public function _get_possible()
     {
         $possible = array();
-        for ($n = 1; $n <= 9; $n++) {
-            for ($y = 1; $y <= 9; $y++) {
-                for ($x = 1; $x <= 9; $x++) {
-                    if ($this->s[$y][$x]) {
+        for ($number = 1; $number <= 9; $number++) {
+            for ($row = 1; $row <= 9; $row++) {
+                for ($column = 1; $column <= 9; $column++) {
+                    if ($this->solved[$row][$column]) {
                         continue;
                     }
-                    $c = $this->_get_c_id($y, $x);
+                    $c = $this->_get_square_from_row_column($row, $column);
                     if (
-                        !in_array($n, $this->y[$y]) &&
-                        !in_array($n, $this->x[$x]) &&
-                        !in_array($n, $this->c[$c['cube']])
+                        !in_array($number, $this->rows[$row]) &&
+                        !in_array($number, $this->columns[$column]) &&
+                        !in_array($number, $this->squares[$c['cube']])
                     ) {
-                        $possible[$y][$x][] = $n;
+                        $possible[$row][$column][] = $number;
                     }
                 }
             }
@@ -263,13 +263,13 @@ class Model
     /**
      * Sets a cell that has been found
      */
-    public function _set_s_cell($y, $x, $n) 
+    public function _set_cell_solved($row, $column, $number) 
     {
-        $this->s[$y][$x] = $n;
-        $this->y[$y][$x] = $n;
-        $this->x[$x][$y] = $n;
-        $c = $this->_get_c_id($y, $x);
-        $this->c[$c['cube']][$c['cell']] = $n;
+        $this->solved[$row][$column] = $number;
+        $this->rows[$row][$column] = $number;
+        $this->columns[$column][$row] = $number;
+        $c = $this->_get_square_from_row_column($row, $column);
+        $this->squares[$c['cube']][$c['cell']] = $number;
     }
 
     /**
@@ -279,14 +279,14 @@ class Model
      */
     public function _check_possible_single($possible)
     {
-        foreach ($possible as $y => $ya) {
-            foreach ($ya as $x => $xa) {
+        foreach ($possible as $row => $columns) {
+            foreach ($columns as $column => $numbers) {
                 // Check if there is only one possible answer for cell
-                if (count($xa) == 1) {
-                    $this->_set_s_cell($y, $x, $xa[0]);
-                    unset($possible[$y][$x]);
-                    if (empty($possible[$y])) {
-                        unset ($possible[$y]);
+                if (count($numbers) == 1) {
+                    $this->_set_cell_solved($row, $column, $numbers[0]);
+                    unset($possible[$row][$column]);
+                    if (empty($possible[$row])) {
+                        unset ($possible[$row]);
                     }
                     continue;
                 }
@@ -299,29 +299,29 @@ class Model
      * 
      * @param Array $possible
      */
-    public function _check_possible_y($possible)
+    public function _check_possible_in_row($possible)
     {
         // Check if only one instance of n exists for row
-        for ($y = 1; $y <= 9; $y++) {
-            if (!array_key_exists($y, $possible)) {
+        for ($row = 1; $row <= 9; $row++) {
+            if (!array_key_exists($row, $possible)) {
                 continue;
             }
-            for ($n = 1; $n <= 9; $n++) {
-                $fx = $fy = $count = 0;
-                foreach ($possible[$y] as $x => $xa) {
-                    foreach ($xa as $p) {
-                        if ($p == $n) {
-                            $fx = $x;
-                            $fy = $y;
+            for ($number = 1; $number <= 9; $number++) {
+                $found_column = $found_row = $count = 0;
+                foreach ($possible[$row] as $column => $numbers) {
+                    foreach ($numbers as $possible_number) {
+                        if ($possible_number == $number) {
+                            $found_column = $column;
+                            $found_row = $row;
                             $count++;
                         }
                     }
                 }
                 if ($count == 1) {
-                    $this->_set_s_cell($fy, $fx, $n);
-                    unset($possible[$fy][$fx]);
-                    if (empty($possible[$fy])) {
-                        unset($possible[$fy]);
+                    $this->_set_cell_solved($found_row, $found_column, $number);
+                    unset($possible[$found_row][$found_column]);
+                    if (empty($possible[$found_row])) {
+                        unset($possible[$found_row]);
                         continue(2);
                     }
                 }                
@@ -334,29 +334,29 @@ class Model
      * 
      * @param Array $possible
      */
-    public function _check_possible_c($possible) 
+    public function _check_possible_in_square($possible) 
     {
-        foreach ($possible as $y => $ya) {
-            foreach ($ya as $x => $xa) {
-                $c = $this->_get_c_id($y, $x);
-                for ($n = 1; $n <= 9; $n++) {
-                    $fx = $fy = $count = 0;
-                    foreach ($c['cells'] as $cell) {
+        foreach ($possible as $row => $columns) {
+            foreach ($columns as $column => $cells) {
+                $square = $this->_get_square_from_row_column($row, $column);
+                for ($number = 1; $number <= 9; $number++) {
+                    $found_column = $found_row = $count = 0;
+                    foreach ($square['cells'] as $cell) {
                         if (array_key_exists($cell['y'], $possible) && array_key_exists($cell['x'], $possible[$cell['y']])) {
-                            foreach ($possible[$cell['y']][$cell['x']] as $p) {
-                                if ($p == $n) {
-                                    $fx = $cell['x'];
-                                    $fy = $cell['y'];
+                            foreach ($possible[$cell['y']][$cell['x']] as $possible_number) {
+                                if ($possible_number == $number) {
+                                    $found_column = $cell['x'];
+                                    $found_row = $cell['y'];
                                     $count++;
                                 }
                             }
                         }
                     }
                     if ($count == 1) {
-                        $this->_set_s_cell($fy, $fx, $n);
-                        unset ($possible[$fy][$fx]);
-                        if (empty($possible[$fy])) {
-                            unset($possible[$fy]);
+                        $this->_set_cell_solved($found_row, $found_column, $number);
+                        unset ($possible[$found_row][$found_column]);
+                        if (empty($possible[$found_row])) {
+                            unset($possible[$found_row]);
                             continue(2);
                         }
                     }
@@ -370,36 +370,36 @@ class Model
      * 
      * @param Array $possible
      */
-    public function _check_possible_x($possible)
+    public function _check_possible_in_column($possible)
     {
         // Create X Y array from Y X array
-        $possible_x = array();
-        foreach ($possible as $y => $ya) {
-            foreach ($ya as $x => $xa) {
-                $possible_x[$x][$y] = $xa;
+        $possible_row = array();
+        foreach ($possible as $row => $columns) {
+            foreach ($columns as $column => $numbers) {
+                $possible_row[$column][$row] = $numbers;
             }
         }
         // Check if only one instance of n exists for col
-        for ($x = 1; $x <= 9; $x++) {
-            if (!array_key_exists($x, $possible_x)) {
+        for ($column = 1; $column <= 9; $column++) {
+            if (!array_key_exists($column, $possible_row)) {
                 continue;
             }
-            for ($n = 1; $n <= 9; $n++) {
-                $fx = $fy = $count = 0;
-                foreach ($possible_x[$x] as $y => $ya) {
-                    foreach ($ya as $p) {
-                        if ($p == $n) {
-                            $fx = $x;
-                            $fy = $y;
+            for ($number = 1; $number <= 9; $number++) {
+                $found_column = $found_row = $count = 0;
+                foreach ($possible_row[$column] as $row => $columns) {
+                    foreach ($columns as $possible_number) {
+                        if ($possible_number == $number) {
+                            $found_column = $column;
+                            $found_row = $row;
                             $count++;
                         }
                     }
                 }
                 if ($count == 1) {
-                    $this->_set_s_cell($fy, $fx, $n);
-                    unset ($possible_x[$fx][$fy]);
-                    if (empty($possible_x[$fx])) {
-                        unset($possible_x[$fx]);
+                    $this->_set_cell_solved($found_row, $found_column, $number);
+                    unset ($possible_row[$found_column][$found_row]);
+                    if (empty($possible_row[$found_column])) {
+                        unset($possible_row[$found_column]);
                         continue(2);
                     }
                 }
@@ -420,38 +420,38 @@ class Model
     {
         $start = floor(microtime(true) * 1000);
         for ($i = 1; $i <= 1000; $i++) {
-            $pre_s = $this->s;
+            $pre_solved = $this->solved;
             $this->_check_possible_single(
                 $this->_get_possible()
             );
-            $this->_check_possible_y(
+            $this->_check_possible_in_row(
                 $this->_get_possible()
             );
-            $this->_check_possible_x(
+            $this->_check_possible_in_column(
                 $this->_get_possible()
             );
-            $this->_check_possible_c(
+            $this->_check_possible_in_square(
                 $this->_get_possible()
             );
 
             if ($this->is_solved()) {
                 $end = floor(microtime(true) * 1000);
                 return array(
-                    'resp' => $this->s,
+                    'resp' => $this->solved,
                     'mode' => 'fully_solved',
                     'iterations' => $i,
                     'time' => $end - $start,
                 );
-                return $this->s;
+                return $this->solved;
             }
 
-            if ($pre_s == $this->s) {
+            if ($pre_solved == $this->solved) {
                 // No changes, cannot solve
                 $end = floor(microtime(true) * 1000);
 
-                if ($this->p == $this->s) {
+                if ($this->puzzle == $this->solved) {
                     return array(
-                        'resp' => $this->s,
+                        'resp' => $this->solved,
                         'mode' => 'unsolved',
                         'iterations' => $i,
                         'time' => $end - $start,
@@ -459,7 +459,7 @@ class Model
                 }
         
                 return array(
-                    'resp' => $this->s,
+                    'resp' => $this->solved,
                     'mode' => 'partially_solved',
                     'iterations' => $i,
                     'time' => $end - $start,
